29c472abab9dc450681d7bb20bdb6f5d072af041
etherc <etherc@etherc.eu>
2026-02-05 22:04:29 +0300

    UI: switch default network to mainnet

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -727,7 +727,7 @@ export class AppData {
   }
   static defaultNetwork(): NetworkType {
     // @ts-ignore
-    return import.meta.env.DEV ? NetworkType.Regtest : NetworkType.Testnet;
+    return import.meta.env.DEV ? NetworkType.Regtest : NetworkType.Mainnet;
   }
 }
 
