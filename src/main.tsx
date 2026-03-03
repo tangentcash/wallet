b1d775b70db3abf749289480d52dec8a07bf2952
etherealcardinal <etherc@etherc.eu>
2026-03-03 21:27:35 +0300

    UI: test

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,10 @@ import './styles/colors.css';
 import './styles/customs.css';
 import BigNumber from "bignumber.js";
 import { AppData } from "./core/app";
+import { RPC } from "tangentsdk";
+
+const data = "7e-15";
+console.log(RPC.fetchObject(data).plus(1).toString());
 
 console.log("%cSTOP! Do not enter any commands you do not understand, your wallet might be at risk.", "font: 2em sans-serif; padding: 6px 12px; color: yellow; border-radius: 8px; background-color: red;");
 BigNumber.config({ DECIMAL_PLACES: 18, ROUNDING_MODE: 1 });
